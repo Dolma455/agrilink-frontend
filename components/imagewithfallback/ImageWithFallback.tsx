e57c6dffc6adae3ca18a21d27ffcd830e63e2539
Dolma455 <dolma1918@gmail.com>
2025-11-26 00:10:19 +0545

    fix: Images changed

--- components/imagewithfallback/ImageWithFallback.tsx
+++ components/imagewithfallback/ImageWithFallback.tsx
@@ -2,11 +2,16 @@ import Image from "next/image";
 import { useState } from "react";
 
 function ImageWithFallback({ src, fallbackSrc, alt, className }: any) {
-  const [imgSrc, setImgSrc] = useState(src);
+  const [imgSrc, setImgSrc] = useState(src || fallbackSrc);
+
+  // Don't render if both src and fallbackSrc are empty
+  if (!imgSrc && !fallbackSrc) {
+    return null;
+  }
 
   return (
     <Image
-      src={imgSrc}
+      src={imgSrc || fallbackSrc}
       alt={alt}
       className={className}
       width={1080}
